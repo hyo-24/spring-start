c6da59e3a5729a11dd0124810ed68d2ae51c654d
Elena Lee <tbfjs3976@icloud.com>
2026-02-22 15:42:50 +0900

    02/22

--- hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -17,8 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class MemberServiceIntegrationTest {
 
     @Autowired MemberService memberService;
-    @Autowired
-    MemberRepository memberRepository;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void 회원가입() {
